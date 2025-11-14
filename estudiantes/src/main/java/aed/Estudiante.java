7398cec2c58515ac904d5c12f5db72ddbc8e1b79
TotoMC <52181149+TotoMC-13@users.noreply.github.com>
2025-11-14 13:54:53 -0300

    Refactor Estudiante class constructor and add compareTo

--- estudiantes/src/main/java/aed/Estudiante.java
+++ estudiantes/src/main/java/aed/Estudiante.java
@@ -4,24 +4,51 @@ public class Estudiante {
     int[] _examen; // Arreglo de tamaño R que almacena las respuestas (o -1 si está enblanco)
     double _puntaje; // El puntaje actual del estudiante
     // Es un puntero a la posición del estudiante en el _puntajes Min-Heap, permitiendo actualizaciones en O(log E).
-    // HeapHandle _posicionEnHeap; 
+    // HeapHandle _posicionEnHeap;
+    int _id;
     int _fila;
     int _columna;
     boolean _esSospechoso;
     boolean _yaEntrego;
 
-    public Estudiante(int cantRespuestas, int fila, int columna) {
+    public Estudiante(int cantRespuestas, int id, int fila, int columna, boolean esSospechoso, boolean yaEntrego) {
         // Creamos la lista de len = cantRespuestas y la llenamos de 2
         // Que seria que no respondio
 
         _examen = new int[cantRespuestas];
+        _id = id;
+        _puntaje = (double) 0;
+        _fila = fila;
+        _columna = columna;
+        _esSospechoso = esSospechoso;
+        _yaEntrego = yaEntrego;
 
         for(int i = 0; i < cantRespuestas; i++) {
             _examen[i] = -1;
         }
 
-        _puntaje = 0;
-        _fila = 0;
-        _columna = 0;
-    } 
+    }
+
+    public int compareTo(Estudiante est) {
+        if (this._yaEntrego && est._yaEntrego) { // Si ambos entregaron
+            return this._id - est._id;
+        } else {
+            if (this._yaEntrego || est._yaEntrego) { // Si alguno entrego
+                if (this._yaEntrego) { // Si yo entregue, soy mayor
+                    return 1;
+                } else { // Si no, soy menor
+                    return 1;
+                }
+            } else if (this._puntaje != est._puntaje) { // Si son distintos los puntajes, puedo compararlos
+                return Double.compare(this._puntaje, est._puntaje);
+            }
+        }
+
+        // Si todo falla, comparo ids
+        return this._id - est._id;
+    }
+
+    public void setPuntaje(double puntaje) {
+        _puntaje = puntaje;
+    }
 }
