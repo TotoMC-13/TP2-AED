1f1a4f49f5d9e7d88d252e3257a658d1c2b88d4e
TotoMC <52181149+TotoMC-13@users.noreply.github.com>
2025-11-14 13:54:09 -0300

    Add unit tests for Estudiante comparison

---
+++ estudiantes/src/test/java/aed/estudianteTest.java
@@ -0,0 +1,75 @@
+package aed;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import java.util.Arrays;
+
+class EstudianteTest {
+    @Test
+    void menorNota() {
+        // La nota default es 0
+        Estudiante e1 = new Estudiante(10, 10, 2, 2, false, true);
+        Estudiante e2 = new Estudiante(5, 11, 2, 2, false, true);
+        e2.setPuntaje((double)10);
+        assertEquals(e1.compareTo(e2), -1);
+    }
+
+    @Test
+    void mayorNota() {
+        // La nota default es 0
+        Estudiante e1 = new Estudiante(10, 10, 2, 2, false, true);
+        Estudiante e2 = new Estudiante(5, 11, 2, 2, false, true);
+        e1.setPuntaje((double) 10);
+        assertEquals(e1.compareTo(e2), 1);
+    }
+
+    @Test
+    void mismaNotaMenorId() {
+        // La nota default es 0
+        Estudiante e1 = new Estudiante(10, 10, 2, 2, false, false);
+        Estudiante e2 = new Estudiante(5, 11, 2, 2, false, false);
+        assertEquals(e1.compareTo(e2), -1);
+    }
+
+    @Test
+    void mismaNotaMayorId() {
+        // La nota default es 0
+        Estudiante e1 = new Estudiante(10, 12, 2, 2, false, false);
+        Estudiante e2 = new Estudiante(5, 11, 2, 2, false, false);
+        assertEquals(e1.compareTo(e2), 1);
+    }
+
+    @Test
+    void yaEntregue() {
+        // La nota default es 0
+        Estudiante e1 = new Estudiante(10, 12, 2, 2, false, true);
+        Estudiante e2 = new Estudiante(5, 11, 2, 2, false, false);
+        assertEquals(e1.compareTo(e2), 1);
+    }
+
+    @Test
+    void yaEntrego() {
+        // La nota default es 0
+        Estudiante e1 = new Estudiante(10, 12, 2, 2, false, false);
+        Estudiante e2 = new Estudiante(5, 11, 2, 2, false, true);
+        assertEquals(e1.compareTo(e2), 1);
+    }
+
+    @Test
+    void ambosEntregaronMenorId() {
+        // La nota default es 0
+        Estudiante e1 = new Estudiante(10, 10, 2, 2, false, true);
+        Estudiante e2 = new Estudiante(5, 11, 2, 2, false, true);
+        assertEquals(e1.compareTo(e2), -1);
+    }
+
+    @Test
+    void ambosEntregaronMayorId() {
+        // La nota default es 0
+        Estudiante e1 = new Estudiante(10, 12, 2, 2, false, true);
+        Estudiante e2 = new Estudiante(5, 11, 2, 2, false, true);
+        assertEquals(e1.compareTo(e2), 1);
+    }
+}
