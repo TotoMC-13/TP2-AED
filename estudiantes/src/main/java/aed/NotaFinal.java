776b2bceaff74d95dc707bf532ca8ea2a3ccd33c
andresfalt <faltlhauserandres@gmail.com>
2025-11-23 22:14:23 -0300

    Refactor compareTo method for NotaFinal class

--- estudiantes/src/main/java/aed/NotaFinal.java
+++ estudiantes/src/main/java/aed/NotaFinal.java
@@ -10,9 +10,22 @@ public class NotaFinal implements Comparable<NotaFinal> {
     }
 
     public int compareTo(NotaFinal otra){
-        if (otra._id != this._id){
-            return this._id - otra._id;
+        int notaComparison = Double.compare(otra._nota, this._nota);
+
+        // Comparar por nota (decreceinte)
+        if (notaComparison != 0) {
+            return notaComparison;
         }
-        return Double.compare(this._nota, otra._nota);
+
+        // Si las notas son iguales, comparar por ID (decreceinte)
+        return Integer.compare(otra._id, this._id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        NotaFinal other = (NotaFinal) obj;
+        return Double.compare(other._nota, _nota) == 0 && _id == other._id;
     }
 }
